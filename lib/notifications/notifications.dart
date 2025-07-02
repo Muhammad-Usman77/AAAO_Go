7dab2f319f8a0738d6ec378a29e24b2d92901121
Muhammad Usman <awanask086@gmail.com>
2025-07-02 15:56:45 +0500

    notificationpage

--- lib/notifications/notifications.dart
+++ lib/notifications/notifications.dart
@@ -18,17 +18,8 @@ class _NotificationspageState extends State<Notificationspage> {
           backgroundColor: AppColor.appclr2,
           body: Column(
             children: [
-              BackBtnPInfo(
-                icons: Icons.arrow_back_ios_new,
-                text1: 'Notifications',
-                text2: 'ClearAll',
-              ),
-              NotificationWidget(
-                icons1: Icons.notifications_none_outlined,
-                text1: 'Zoe send You a Message',
-                icons2: Icons.delete_outline_outlined,
-                icons3: Icons.watch_later_outlined,
-              )
+             Text('usman', style:TextStyle(color: AppColor.appclr),) 
+             
             ],
           )),
     );
